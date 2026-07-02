e13e043999e8df9afb4141606a6dd7f47165acda
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-07-02 18:24:12 +0000

    chudmaxxing

--- 5.structs/src/main.rs
+++ 5.structs/src/main.rs
@@ -1,20 +1,115 @@
-// fn main() {
-//     println!("Hello, world!");
+// // fn main() {
+// //     println!("Hello, world!");
+// // }
+
+
+// // struct User {
+// //     active: bool,
+// //     username: String,
+// //     email: String,
+// //     sign_in_count: u64,
+// // }
+
+// // fn main() {
+// //     let user1 = User {
+// //         active: true,
+// //         username: String::from("someusername123"),
+// //         email: String::from("someone@example.com"),
+// //         sign_in_count: 1,
+// //     };
+// // }
+
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
 // }
 
+// fn build_user(email: String, username: String) -> User {
+//     User {
+//         active: true,
+//         username: username,
+//         email: email,
+//         sign_in_count: 1,
+//     }
+// }
 
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+// fn main() {
+//     let user1 = build_user(
+//         String::from("someone@example.com"),
+//         String::from("someusername123"),
+//     );
+// }
+
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+// }
+
+// fn main() {
+//     // --snip--
+
+//     let user1 = User {
+//         email: String::from("someone@example.com"),
+//         username: String::from("someusername123"),
+//         active: true,
+//         sign_in_count: 1,
+//     };
+
+//     let user2 = User {
+//         email: String::from("another@example.com"),
+//         ..user1
+//     };
+// }
+
+// fn main() {
+//     let rect1 = (30, 50);
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(rect1)
+//     );
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(&rect1)
+//     );
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
 fn main() {
-    let user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
     };
+
+    println!("rect1 is {rect1:?}");
 }
