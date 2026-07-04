db5ceb05ce243b12657bb379e0fa5f0a75dfe9ed
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-07-04 13:34:33 +0000

    chudmaxxing day 2

--- 5.structs/src/main.rs
+++ 5.structs/src/main.rs
@@ -99,17 +99,116 @@
 //     rectangle.width * rectangle.height
 // }
 
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!("rect1 is {rect1:?}");
+// }
+// #[derive(Debug)]
+// struct Rectangle{
+//     height : u32,
+//     width : u32,
+// }
+
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+// }
+
+// fn main(){
+//     let rec = Rectangle {
+//         height: 30,
+//         width : 20,
+//     };
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         rec.area()
+//     );
+// }
+
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// impl Rectangle {
+//     fn width(&self) -> bool {
+//         self.width > 0
+//     }
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     if rect1.width() {
+//         println!("The rectangle has a nonzero width; it is {}", rect1.width);
+//     }
+// }
+
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+//     let rect2 = Rectangle {
+//         width: 10,
+//         height: 40,
+//     };
+//     let rect3 = Rectangle {
+//         width: 60,
+//         height: 45,
+//     };
+
+//     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+//     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+// }
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
 
-    println!("rect1 is {rect1:?}");
+fn main() {
+    let sq = Rectangle::square(3);
 }
