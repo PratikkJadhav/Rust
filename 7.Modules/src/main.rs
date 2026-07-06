44f8871907d232eaafdb7fbbe39306e6b1a8a2c3
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-07-06 16:17:02 +0000

    fatchudmaxxing

--- 7.Modules/src/main.rs
+++ 7.Modules/src/main.rs
@@ -27,3 +27,17 @@
 // pub fn eat_at_restaurant() {
 //     hosting::add_to_waitlist();
 // }
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+use crate::front_of_house::hosting::add_to_waitlist;
+
+pub fn eat_at_restaurant() {
+    add_to_waitlist();
+}
+
+fn main(){}
