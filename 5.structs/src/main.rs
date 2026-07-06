8eabfa0fb475868a926fa296497b8755c9684b04
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-07-06 14:42:38 +0000

    fatchudmaxxing

--- 5.structs/src/main.rs
+++ 5.structs/src/main.rs
@@ -213,22 +213,111 @@
 //     let sq = Rectangle::square(3);
 // }
 
+// fn main() {
+//     enum Message {
+//         Quit,
+//         Move { x: i32, y: i32 },
+//         Write(String),
+//         ChangeColor(i32, i32, i32),
+//     }
+
+//     impl Message {
+//         fn call(&self) {
+//             // method body would be defined here
+//         }
+//     }
+
+//     let m = Message::Write(String::from("hello"));
+//     m.call();
+
+//     let m: Option<_> = None;
+// }
+
+
+// enum States {
+//     Alabama,
+//     Newyork,
+//     Mancity,
+// }
+
+
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(States),
+// }
+
+// fn value(coin: Coin) -> u8{
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Dime => 10,
+//         Coin::Nickel => 11,
+//         Coin::Quarter => 12,
+//     }
+// }
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => {
+//             println!("Lucky penny!");
+//             1
+//         }
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter => 25,
+//     }
+// }
+
+// fn main(){}
+
+
+// fn main(){
+//     fn plus_one(x:Option<i32>)->Option<i32>{
+//         match x {
+//             None => None,
+//             Some(i) => Some(i + 1),
+//         }
+//     }
+
+//     let one = Some(1);
+//     let two = plus_one(one);
+
+// }
+
+// fn main() {
+//     let dice_roll = 9;
+//     match dice_roll {
+//         3 => add_fancy_hat(),
+//         7 => remove_fancy_hat(),
+//         other => move_player(other),
+//     }
+
+//     fn add_fancy_hat() {}
+//     fn remove_fancy_hat() {}
+//     fn move_player(num_spaces: u8) {}
+// }
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
-    enum Message {
-        Quit,
-        Move { x: i32, y: i32 },
-        Write(String),
-        ChangeColor(i32, i32, i32),
+    let coin = Coin::Penny;
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {state:?}!");
+    } else {
+        count += 1;
     }
-
-    impl Message {
-        fn call(&self) {
-            // method body would be defined here
-        }
-    }
-
-    let m = Message::Write(String::from("hello"));
-    m.call();
-
-    let m: Option<_> = None;
 }
