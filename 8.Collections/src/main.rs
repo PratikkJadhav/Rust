0881d90c993c489833ae72eddb96595730c8dae0
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-07-07 09:08:37 +0000

    rust

--- 8.Collections/src/main.rs
+++ 8.Collections/src/main.rs
@@ -1,26 +1,100 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
+use std::collections::{HashMap, hash_map};
+
+// fn main(){
+//     let mut v: Vec<i32> = Vec::new();
+//     let mut a = vec![1,2,3,4,5];
+
+//     v.push(1);
+//     v.push(1);
+//     v.push(1);
+//     v.push(1);
+//     v.push(1);
 
 
-// fn main() {
-//     let v = vec![1, 2, 3, 4, 5];
+//     // let third = a.get(1);
+//     // match third {
+//     //     Some(third) => print!("{third}" ),
+//     //     _ => print!("not third"),
+//     // }
 
-//     let third: &i32 = &v[2];
-//     println!("The third element is {third}");
+//     // let x = &v[100];
+//     // let y = v.get(100);
 
-//     let third: Option<&i32> = v.get(2);
-//     match third {
-//         Some(third) => println!("The third element is {third}"),
-//         None => println!("There is no third element."),
+//     // for i in &mut a {
+//     //     *i += i;
+//     // }
+
+//     enum SpreadsheetCell {
+//         Int(i32),
+//         Float(f64),
+//         Text(String),
 //     }
+
+//     let row = vec![
+//         SpreadsheetCell::Int(3),
+//         SpreadsheetCell::Text(String::from("blue")),
+//         SpreadsheetCell::Float(10.12),
+//     ];
+
 // }
 
-fn main() {
-    use std::collections::HashMap;
+// fn main(){
+//     let mut s21 = String::new();
+//     let data = "initial comment";
+//     let five = data.to_string();
+//     let s = String::from("initial contents");
 
-    let mut scores = HashMap::new();
+//     let mut s = String::from("foo");
+//     s.push_str("bar");
 
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+//     let s1 = String::from("Hello, ");
+//     let s2 = String::from("world!");
+//     let s3 = s1 + &s2;
+
+//     for c in "Зд".chars() {
+//         println!("{c}");
+//     }
+
+//     for b in "Зд".bytes() {
+//         println!("{b}");
+//     }
+
+// }
+
+fn main(){
+    // let mut map = HashMap::new();
+
+    // map.insert(String::from("Blue"), 10);
+    // map.insert(String::from("Red"), 10);
+
+    // let mut scores = HashMap::new();
+
+    // scores.insert(String::from("Blue"), 10);
+    // scores.insert(String::from("Yellow"), 50);
+
+    // let team_name = String::from("Blue");
+    // let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    // for (key, value) in &scores {
+    //     println!("{key}: {value}");
+    // }
+
+    // let mut scores = HashMap::new();
+    // scores.insert(String::from("Blue"), 10);
+
+    // scores.entry(String::from("Yellow")).or_insert(50);
+    // scores.entry(String::from("Blue")).or_insert(50);
+
+    // println!("{scores:?}");
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{map:?}");
 }
