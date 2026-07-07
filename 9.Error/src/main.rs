801ae1e7f34dd1f85793723d7d5a07375dc28725
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-07-07 12:09:36 +0000

    valorant

--- 9.Error/src/main.rs
+++ 9.Error/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::File;
+use std::io::ErrorKind;
+// fn main() {
+//     let greeting_file_result = File::open("hello.txt");
+
+//     let greeting_file = match greeting_file_result {
+//         Ok(file) => file,
+//         Err(error) => panic!("Problem opening the file: {error:?}"),
+//     };
+// }
+
+
+// fn main() {
+//     let greeting_file_result = File::open("hello.txt");
+
+//     let greeting_file = match greeting_file_result {
+//         Ok(file) => file,
+//         Err(error) => match error.kind() {
+//             ErrorKind::NotFound => match File::create("hello.txt") {
+//                 Ok(fc) => fc,
+//                 Err(e) => panic!("Problem creating the file: {e:?}"),
+//             },
+//             _ => {
+//                 panic!("Problem opening the file: {error:?}");
+//             }
+//         },
+//     };
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let greeting_file = File::open("hello.txt").unwrap();
 }
