92bdbde68bb789b918e402d5320c08aaa06c9b63
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-07-07 18:20:16 +0000

    cli app part1&2

---
+++ minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::fs;
+
+fn main(){
+    let args:Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filepath = &args[2];
+
+    println!("Searching for {query}");
+    println!("In the file {filepath}");
+
+    let contents = fs::read_to_string(filepath).expect("Should have been able to read the file");
+
+    println!("With text \n{contents}");
+
+}
