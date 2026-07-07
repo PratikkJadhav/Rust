e6dc2c685ab55f57a3548f84af3c95ef0c700dcf
PratikkJadhav <pratikjadhav032005@gmail.com>
2026-07-07 17:12:44 +0000

    lifetimes

--- 10.Generic/src/main.rs
+++ 10.Generic/src/main.rs
@@ -1,3 +1,32 @@
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// fn main() {
+//     let integer = Point { x: 5, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+// }
+
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+
+// fn main() {
+//     let both_integer = Point { x: 5, y: 10 };
+//     let both_float = Point { x: 1.0, y: 4.0 };
+//     let integer_and_float = Point { x: 5, y: 4.0 };
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() { x } else { y }
 }
